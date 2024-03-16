3d44ff3b2a6ab16cd4cc224a3c82b0937e5f7709
l3miage-khalili <khalilbrahimgoukouni@gmail.com>
2024-03-16 04:58:47 +0100

    réalise la modélisation en JPA des entités et rélations de l'exo3-TP2

--- server/src/main/java/fr/uga/l3miage/tp2/exo3/models/ClientEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp2/exo3/models/ClientEntity.java
@@ -1,2 +1,24 @@
-package fr.uga.l3miage.tp2.exo3.models;public class ClientEntity {
+package fr.uga.l3miage.tp2.exo3.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "client")
+public class ClientEntity {
+    @Id
+    @Column(name = "id")
+    private Long id ;
+
+    @Column(name = "nom")
+    private String nom ;
+
+    @Column(name = "email")
+    private String email ;
+
+    @OneToMany(mappedBy = "clientEntity")
+    private Set<CommandeEntity> commandeEntities ;
+
+    @OneToOne(mappedBy = "clientEntity")
+    private AdresseEntity adresseEntity ;
 }
